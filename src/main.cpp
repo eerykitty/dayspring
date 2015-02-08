78e9f5b7243f5bd6c0294d37b9d3c8c30b29f78c
spijdar <joseph.allen.ilive@gmail.com>
2015-02-07 19:35:25 -0600

    Initial source code.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Dayspring - mk 3.
+ * 
+ * Versioning scheme:
+ * 	three versions; major, minor, and revision, i.e. 1.2.31 would be the first major release, the second minor release, and the thirty first revision.
+ *
+ * Milestones:
+ * 	To 0.1.0:
+ */
+
+int main (int argc, char** argv)
+{
+	
+}
